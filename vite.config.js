2466ebba4df2c3cbbb2c023a48c08cdecc791d7a
Arun jR <cannon.arun1234@gmail.com>
2025-11-06 16:53:00 +0545

    does it work

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +14,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-'@': path.resolve('/Users/arun/Documents/project/Aangan_Restro-main', 'src')    },
+      '@': path.resolve(__dirname, 'src')
+    },
   },
 })
